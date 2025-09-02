86ddc2bfb09e83a11339298f4df194d1aa83574d
Carrotist <romark99@gmail.com>
2025-09-02 11:38:12 +0300

    Feat: Icon component, sprite fix

--- src/components/Advantages/AdvantagesCard.js
+++ src/components/Advantages/AdvantagesCard.js
@@ -1,12 +1,11 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import Icon from '../Icon/Icon';
 
 const AdvantagesCard = ({ icon, text }) => {
   return (
     <div className='advantages-card col-lg-3 col-md-6 col-sm-6'>
-      <svg className='advantages-card__svg'>
-        <use href={`src/icons/sprite.svg#${icon}`}></use>
-      </svg>
+      <Icon icon={icon}></Icon>
       <p className='advantages-card__text'>{text}</p>
     </div>
   );
