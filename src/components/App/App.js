0804df1fca7379f6abbeda992baff0283cecf424
Carrotist <romark99@gmail.com>
2025-09-07 19:36:08 +0300

    Feat: Available Hotels design improved to Homes Guests

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,12 @@ import Advantages from '../Advantages/Advantages';
 import HomesGuests from '../HomesGuestsLove/HomesGuests';
 import { hotelsData } from '../../utils/hotelsData';
 import TripHouse from '../TripHouse/TripHouse';
-import AvailableHotels from '../HomesGuestsLove/AvailableHotels';
 
 export const App = () => {
   return (
     <>
       <TripHouse />
-      <AvailableHotels />
+      <HomesGuests hotels={hotelsData} isAvailableHotels={true} />
       <Advantages />
       <HomesGuests hotels={hotelsData} />
     </>
