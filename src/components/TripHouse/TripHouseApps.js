183c4c17493d22064334803a7a9c2b7ad7d458f4
Carrotist <romark99@gmail.com>
2025-09-07 19:22:37 +0300

    Feat: TripHouse components

---
+++ src/components/TripHouse/TripHouseApps.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TripHouseApps = () => {
+  return (
+    <div className='triphouse-apps'>
+      <img
+        src='/icons/google-play-logo.png'
+        alt='Get it on Google Play button'
+      />
+      <img src='/icons/app-store-logo.png' alt='Get it on App Store button' />
+    </div>
+  );
+};
+
+export default TripHouseApps;
